cc5fbedcf075529ba10ae00ff6b6ae7efd83eecb
Ryohei0Otsuka <monoce0cream@hotmail.co.jp>
2024-06-11 23:21:55 +0900

    修正

--- JS/script.js
+++ JS/script.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
             $('#overlay, .modal-window').fadeOut();
             $('.modal-window input[type="radio"]').prop('checked', false);
             $('.comment').val('');
-            $('.time01, .time02').val('-- : --');
+            $('.time01, .time02').val('--：--');
             $('.error-message01, .error-message02, .error-message03').css('visibility', 'hidden');
             $('.comment, .time01, .time02').prop('disabled', true).css('border-color', '#c2bcbc');
         });
@@ -63,42 +63,105 @@ $(document).ready(function () {
                 if (status != '外出') {
                     $('.update').prop('disabled', false).css('background-color', '#2196f3');
                     $('.comment, .time01, .time02').prop('disabled', true).css('border-color', '#c2bcbc');
+                    $('.time01h, .time02h').prop('disabled', false);
                     $('.error-message01, .error-message02, .error-message03').css('visibility', 'hidden');
                     $('.comment').val('');
-                    $('.time01').val('-- : --');
-                    $('.time02').val('-- : --');
+
                 } else {
                     $('.comment, .time01, .time02').prop('disabled', false);
                     $('.update').prop('disabled', true).css('background-color', 'red');
                 }
 
-                    $('.comment').on('input', function () {
-                        let commentLength = $('.comment').val().length;
-                        console.log(commentLength);
-                        if (commentLength >= 30 || commentLength == 0) {
-                            $('.update').prop('disabled', true).css('background-color', 'red');
-                            $('.error-message01').css('visibility', 'visible');
-                            $('.comment').css('border-color', '#b00020');
-                        } else {
-                            $('.error-message01').css('visibility', 'hidden');
-                            $('.comment').css('border-color', '#c2bcbc');
+                // コメントフィールドの入力イベントハンドラ
+                $('.comment').on('input', function () {
+                    validateComment();
+                    updateButtonState(); 
+                });
+
+                // 時間01フィールドの入力イベントハンドラ
+                $('.time01').on('input', function () {
+                    validateTimeFields();
+                    updateButtonState(); 
+                });
+
+                // 時間02フィールドの入力イベントハンドラ
+                $('.time02').on('input', function () {
+                    validateTimeFields();
+                    updateButtonState(); 
+                });
+
+                function validateComment() {
+                    let commentLength = $('.comment').val().length;
+
+                    if (commentLength > 30 || commentLength == 0) {
+                        $('.error-message01').css('visibility', 'visible');
+                        $('.comment').css('border-color', '#b00020');
+                    } else {
+                        $('.error-message01').css('visibility', 'hidden');
+                        $('.comment').css('border-color', '#c2bcbc');
+                    }
+                }
+
+                function validateTimeFields() {
+                    $('.time01').on('input', function () {
+                        let time01 = $('.time01').val().replace('：', '');
+                        let time02 = $('.time02').val().replace('：', '');
+                    
+                        if (time02 !== '') { // time02が入力されている場合のみバリデーションを実行
+                            if (time01 >= time02) {
+                                $('.error-message02').css('visibility', 'visible');
+                                $('.time01').css('border-color', '#b00020');
+                                $('.error-message03').css('visibility', 'visible');
+                                $('.time02').css('border-color', '#b00020');
+                            } else {
+                                $('.error-message02').css('visibility', 'hidden');
+                                $('.time01').css('border-color', '#c2bcbc');
+                                $('.error-message03').css('visibility', 'hidden');
+                                $('.time02').css('border-color', '#c2bcbc');
+                            }
                         }
                     });
 
-                    $('.time01, .time02').on('input', function () {
-                        let time01 = $('.time01').val().replace(':', '');
-                        let time02 = $('.time02').val().replace(':', '');
-                        console.log(time02);
-                        if (time02 == '--  --' || time01 >= time02 || time01 == '-- --') {
-                            $('.update').prop('disabled', true).css('background-color', 'red');
-                            $('.error-message02').css('visibility', 'visible');
-                            $('.time02').css('border-color', '#b00020');
-                        } else {
-                            $('.update').prop('disabled', false).css('background-color', '#2196f3');
-                            $('.error-message02, .error-message03').css('visibility', 'hidden');
-                            $('.time01, .time02').css('border-color', '#c2bcbc');
+                    $('.time02').on('input', function () {
+                        let time01 = $('.time01').val().replace('：', '');
+                        let time02 = $('.time02').val().replace('：', '');
+                    
+                        if (time01 !== '') { // time01が入力されている場合のみバリデーションを実行
+                            if (time01 >= time02) {
+                                $('.error-message02').css('visibility', 'visible');
+                                $('.time01').css('border-color', '#b00020');
+                                $('.error-message03').css('visibility', 'visible');
+                                $('.time02').css('border-color', '#b00020');
+                            } else {
+                                $('.error-message02').css('visibility', 'hidden');
+                                $('.time01').css('border-color', '#c2bcbc');
+                                $('.error-message03').css('visibility', 'hidden');
+                                $('.time02').css('border-color', '#c2bcbc');
+                            }
                         }
                     });
+                }
+
+                function updateButtonState() {
+                    // コメントフィールドが条件を満たしているかを確認
+                    let commentLength = $('.comment').val().length;
+                    let commentValid = !(commentLength > 30 || commentLength == 0);
+
+                    // 時間フィールドのバリデーション結果を確認
+                    let time01 = $('.time01').val().replace('：', '');
+                    let time02 = $('.time02').val().replace('：', '');
+                    let timeValid = !(time01 >= time02);
+
+                    // コメントと時間の両方が条件を満たしている場合にのみボタンを有効化
+                    if (commentValid && timeValid) {
+                        $('.update').prop('disabled', false).css('background-color', '#2196f3');
+                    } else {
+                        $('.update').prop('disabled', true).css('background-color', 'red');
+                    }
+                }
+
+
+
             }
         });
     });
--- index.php
+++ index.php
@@ -134,56 +134,9 @@ if ($status == false) {
                             <p class="modal-start-time">開始時刻</p>
                             <div class="time-size time-size01">
                                 <select id="time-input" name="time01" class='time01'>
-                                    <option value="-- : --">-- : --</option>
-                                    <option value="00:00">00:00</option>
-                                    <option value="00:30">00:30</option>
-                                    <option value="01:00">01:00</option>
-                                    <option value="01:30">01:30</option>
-                                    <option value="02:00">02:00</option>
-                                    <option value="02:30">02:30</option>
-                                    <option value="03:00">03:00</option>
-                                    <option value="03:30">03:30</option>
-                                    <option value="04:00">04:00</option>
-                                    <option value="04:30">04:30</option>
-                                    <option value="05:00">05:00</option>
-                                    <option value="05:30">05:30</option>
-                                    <option value="06:00">06:00</option>
-                                    <option value="06:30">06:30</option>
-                                    <option value="07:00">07:00</option>
-                                    <option value="07:30">07:30</option>
-                                    <option value="08:00">08:00</option>
-                                    <option value="08:30">08:30</option>
-                                    <option value="09:00">09:00</option>
-                                    <option value="09:30">09:30</option>
-                                    <option value="10:00">10:00</option>
-                                    <option value="10:30">10:30</option>
-                                    <option value="11:00">11:00</option>
-                                    <option value="11:30">11:30</option>
-                                    <option value="12:00">12:00</option>
-                                    <option value="12:30">12:30</option>
-                                    <option value="13:00">13:00</option>
-                                    <option value="13:30">13:30</option>
-                                    <option value="14:00">14:00</option>
-                                    <option value="14:30">14:30</option>
-                                    <option value="15:00">15:00</option>
-                                    <option value="15:30">15:30</option>
-                                    <option value="16:00">16:00</option>
-                                    <option value="16:30">16:30</option>
-                                    <option value="17:00">17:00</option>
-                                    <option value="17:30">17:30</option>
-                                    <option value="18:00">18:00</option>
-                                    <option value="18:30">18:30</option>
-                                    <option value="19:00">19:00</option>
-                                    <option value="19:30">19:30</option>
-                                    <option value="20:00">20:00</option>
-                                    <option value="20:30">20:30</option>
-                                    <option value="21:00">21:00</option>
-                                    <option value="21:30">21:30</option>
-                                    <option value="22:00">22:00</option>
-                                    <option value="22:30">22:30</option>
-                                    <option value="23:00">23:00</option>
-                                    <option value="23:30">23:30</option>
+                                <?php echo time_options(); ?>
                                 </select>
+                                <input type="hidden" class='time01h' name="time01" value='--：--' disabled>
                                 <div class="error-message02"><i class="fa-regular fa-circle-xmark"></i>時間の入力が正しくありません。
                                 </div>
                             </div>
@@ -192,56 +145,9 @@ if ($status == false) {
                             <p class="modal-end-time">終了時刻</p>
                             <div class="time-size">
                                 <select id="time-input" name="time02" class='time02'>
-                                    <option value="-- : --">-- : --</option>
-                                    <option value="00:00">00:00</option>
-                                    <option value="00:30">00:30</option>
-                                    <option value="01:00">01:00</option>
-                                    <option value="01:30">01:30</option>
-                                    <option value="02:00">02:00</option>
-                                    <option value="02:30">02:30</option>
-                                    <option value="03:00">03:00</option>
-                                    <option value="03:30">03:30</option>
-                                    <option value="04:00">04:00</option>
-                                    <option value="04:30">04:30</option>
-                                    <option value="05:00">05:00</option>
-                                    <option value="05:30">05:30</option>
-                                    <option value="06:00">06:00</option>
-                                    <option value="06:30">06:30</option>
-                                    <option value="07:00">07:00</option>
-                                    <option value="07:30">07:30</option>
-                                    <option value="08:00">08:00</option>
-                                    <option value="08:30">08:30</option>
-                                    <option value="09:00">09:00</option>
-                                    <option value="09:30">09:30</option>
-                                    <option value="10:00">10:00</option>
-                                    <option value="10:30">10:30</option>
-                                    <option value="11:00">11:00</option>
-                                    <option value="11:30">11:30</option>
-                                    <option value="12:00">12:00</option>
-                                    <option value="12:30">12:30</option>
-                                    <option value="13:00">13:00</option>
-                                    <option value="13:30">13:30</option>
-                                    <option value="14:00">14:00</option>
-                                    <option value="14:30">14:30</option>
-                                    <option value="15:00">15:00</option>
-                                    <option value="15:30">15:30</option>
-                                    <option value="16:00">16:00</option>
-                                    <option value="16:30">16:30</option>
-                                    <option value="17:00">17:00</option>
-                                    <option value="17:30">17:30</option>
-                                    <option value="18:00">18:00</option>
-                                    <option value="18:30">18:30</option>
-                                    <option value="19:00">19:00</option>
-                                    <option value="19:30">19:30</option>
-                                    <option value="20:00">20:00</option>
-                                    <option value="20:30">20:30</option>
-                                    <option value="21:00">21:00</option>
-                                    <option value="21:30">21:30</option>
-                                    <option value="22:00">22:00</option>
-                                    <option value="22:30">22:30</option>
-                                    <option value="23:00">23:00</option>
-                                    <option value="23:30">23:30</option>
+                                <?php echo time_options(); ?>
                                 </select>
+                                <input type="hidden" class='time02h' name="time02" value='--：--' disabled>
                                 <div class="error-message03"><i class="fa-regular fa-circle-xmark"></i>時間の入力が正しくありません。
                                 </div>
                             </div>
