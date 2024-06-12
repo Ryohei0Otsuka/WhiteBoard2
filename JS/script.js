21770bf424531b72b8bed1b52fe52cd9b152643e
Ryohei0Otsuka <monoce0cream@hotmail.co.jp>
2024-06-12 18:10:34 +0900

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
             $('.comment').val('');
             $('.time01, .time02').val('--：--');
             $('.error-message01, .error-message02, .error-message03').css('visibility', 'hidden');
-            $('.comment, .time01, .time02').prop('disabled', true).css('border-color', '#c2bcbc');
+            $('.comment, .time01, .time02').css('border-color', '#c2bcbc');
         });
     });
 
@@ -58,112 +58,99 @@ $(document).ready(function () {
         $('.blood').change(function () {
             if ($(this).is(':checked')) {
                 let status = $(this).val();
-                console.log(status);
-
-                if (status != '外出') {
-                    $('.update').prop('disabled', false).css('background-color', '#2196f3');
-                    $('.comment, .time01, .time02').prop('disabled', true).css('border-color', '#c2bcbc');
-                    $('.time01h, .time02h').prop('disabled', false);
-                    $('.error-message01, .error-message02, .error-message03').css('visibility', 'hidden');
-                    $('.comment').val('');
-
-                } else {
-                    $('.comment, .time01, .time02').prop('disabled', false);
-                    $('.update').prop('disabled', true).css('background-color', 'red');
-                }
-
-                // コメントフィールドの入力イベントハンドラ
-                $('.comment').on('input', function () {
-                    validateComment();
-                    updateButtonState(); 
-                });
-
-                // 時間01フィールドの入力イベントハンドラ
-                $('.time01').on('input', function () {
-                    validateTimeFields();
-                    updateButtonState(); 
-                });
-
-                // 時間02フィールドの入力イベントハンドラ
-                $('.time02').on('input', function () {
-                    validateTimeFields();
-                    updateButtonState(); 
-                });
-
-                function validateComment() {
-                    let commentLength = $('.comment').val().length;
-
-                    if (commentLength > 30 || commentLength == 0) {
-                        $('.error-message01').css('visibility', 'visible');
-                        $('.comment').css('border-color', '#b00020');
-                    } else {
-                        $('.error-message01').css('visibility', 'hidden');
-                        $('.comment').css('border-color', '#c2bcbc');
-                    }
-                }
-
-                function validateTimeFields() {
-                    $('.time01').on('input', function () {
-                        let time01 = $('.time01').val().replace('：', '');
-                        let time02 = $('.time02').val().replace('：', '');
-                    
-                        if (time02 !== '') { // time02が入力されている場合のみバリデーションを実行
-                            if (time01 >= time02) {
-                                $('.error-message02').css('visibility', 'visible');
-                                $('.time01').css('border-color', '#b00020');
-                                $('.error-message03').css('visibility', 'visible');
-                                $('.time02').css('border-color', '#b00020');
-                            } else {
-                                $('.error-message02').css('visibility', 'hidden');
-                                $('.time01').css('border-color', '#c2bcbc');
-                                $('.error-message03').css('visibility', 'hidden');
-                                $('.time02').css('border-color', '#c2bcbc');
-                            }
-                        }
-                    });
-
-                    $('.time02').on('input', function () {
-                        let time01 = $('.time01').val().replace('：', '');
-                        let time02 = $('.time02').val().replace('：', '');
-                    
-                        if (time01 !== '') { // time01が入力されている場合のみバリデーションを実行
-                            if (time01 >= time02) {
-                                $('.error-message02').css('visibility', 'visible');
-                                $('.time01').css('border-color', '#b00020');
-                                $('.error-message03').css('visibility', 'visible');
-                                $('.time02').css('border-color', '#b00020');
-                            } else {
-                                $('.error-message02').css('visibility', 'hidden');
-                                $('.time01').css('border-color', '#c2bcbc');
-                                $('.error-message03').css('visibility', 'hidden');
-                                $('.time02').css('border-color', '#c2bcbc');
-                            }
-                        }
-                    });
-                }
-
-                function updateButtonState() {
-                    // コメントフィールドが条件を満たしているかを確認
-                    let commentLength = $('.comment').val().length;
-                    let commentValid = !(commentLength > 30 || commentLength == 0);
-
-                    // 時間フィールドのバリデーション結果を確認
-                    let time01 = $('.time01').val().replace('：', '');
-                    let time02 = $('.time02').val().replace('：', '');
-                    let timeValid = !(time01 >= time02);
-
-                    // コメントと時間の両方が条件を満たしている場合にのみボタンを有効化
-                    if (commentValid && timeValid) {
-                        $('.update').prop('disabled', false).css('background-color', '#2196f3');
-                    } else {
-                        $('.update').prop('disabled', true).css('background-color', 'red');
-                    }
-                }
-
-
-
+                validateStatus(status);
             }
+            updateButtonState();
         });
+
+        $('.comment').on('input', function () {
+            validateComment();
+            updateButtonState();
+        });
+
+
+        $('.time01').on('input', function () {
+            validateTime();
+            updateButtonState();
+        });
+
+
+        $('.time02').on('input', function () {
+            validateTime();
+            updateButtonState();
+        });
+
+
+        function validateStatus(status) {
+            console.log(status);
+
+            if (status != '外出') {
+                $('.update').prop('disabled', false).css('background-color', '#2196f3');
+                $('.comment, .time01, .time02').prop('disabled', true).css('border-color', '#c2bcbc');
+                $('.time01h, .time02h').prop('disabled', false);
+                $('.error-message01, .error-message02, .error-message03').css('visibility', 'hidden');
+                $('.comment').val('');
+                $('.time01, .time02').val('--：--');
+
+            } else {
+                $('.comment, .time01, .time02').prop('disabled', false);
+                $('.update').prop('disabled', true).css('background-color', 'red');
+            }
+        }
+
+        function validateComment() {
+            let commentLength = $('.comment').val().length;
+            console.log(commentLength);
+
+            if (commentLength > 30 || commentLength == 0) {
+                $('.error-message01').css('visibility', 'visible');
+                $('.comment').css('border-color', '#b00020');
+            } else {
+                $('.error-message01').css('visibility', 'hidden');
+                $('.comment').css('border-color', '#c2bcbc');
+            }
+        }
+
+        function validateTime() {
+
+            let time01 = $('.time01').val().replace('：', '');
+            let time02 = $('.time02').val().replace('：', '');
+            console.log(time01);
+            console.log(time02);
+
+            if (time02 != '----') {
+                if (time01 >= time02) {
+                    $('.error-message02').css('visibility', 'visible');
+                    $('.time01').css('border-color', '#b00020');
+                    $('.error-message03').css('visibility', 'visible');
+                    $('.time02').css('border-color', '#b00020');
+                } else {
+                    $('.error-message02').css('visibility', 'hidden');
+                    $('.time01').css('border-color', '#c2bcbc');
+                    $('.error-message03').css('visibility', 'hidden');
+                    $('.time02').css('border-color', '#c2bcbc');
+                }
+            }
+        }
+
+        function updateButtonState() {
+            let commentLength = $('.comment').val().length;
+            let commentValid = !(commentLength > 30 || commentLength == 0);
+            let status = $('.blood:checked').val();
+
+            let time01 = $('.time01').val().replace('：', '');
+            let time02 = $('.time02').val().replace('：', '');
+            let timeValid = !(time01 >= time02);
+            let sValid = (status === '外出');
+            console.log(sValid);
+
+            if (commentValid && timeValid && sValid) {
+                $('.update').prop('disabled', false).css('background-color', '#2196f3');
+            } else {
+                $('.update').prop('disabled', true).css('background-color', 'red');
+            }
+        }
+
     });
 
 });
